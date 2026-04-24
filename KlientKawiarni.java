54ff587e8999bd640068514e11e801f6949a8cc0
KKucyk <krzysztofkucyk@wp.pl>
2026-04-24 12:59:02 +0200

    Create Klient Kawiarni

--- KlientKawiarni.java
+++ KlientKawiarni.java
@@ -1,3 +1,30 @@
 public class KlientKawiarni {
+    private int idKlienta;
+    private String imie;
+    private String nazwisko;
+    private String email;
 
+    public KlientKawiarni(int idKlienta, String imie, String nazwisko, String emial){
+        this.idKlienta = idKlienta;
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.email = emial;
+    }
+
+    public void info(){
+        System.out.println("Id Klienta: "+idKlienta);
+        System.out.println("Imie i nazwisko: "+imie+" "+nazwisko);
+        System.out.println("Emial: "+email);
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+        KlientKawiarni that = (KlientKawiarni) obj;
+        return idKlienta == that.idKlienta;
+    }
 }
