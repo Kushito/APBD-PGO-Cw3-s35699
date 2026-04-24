c5184bf8f13a69861084c1dcc7428c83c6bcf594
KKucyk <krzysztofkucyk@wp.pl>
2026-04-24 12:47:04 +0200

    Create Produkt Menu

--- ProduktMenu.java
+++ ProduktMenu.java
@@ -1,2 +1,42 @@
 public class ProduktMenu {
+    private String kod;
+    private String nazwa;
+    private double cena;
+    private String kategoria;
+    private static int liczbaProduktow;
+
+    public ProduktMenu(String kod, String nazwa, double cena, String kategoria) {
+        this.kod = kod;
+        this.nazwa = nazwa;
+        this.cena = cena;
+        this.kategoria = kategoria;
+        liczbaProduktow++;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+    public String getNazwa() {
+        return nazwa;
+    }
+    public double getCena() {
+        return cena;
+    }
+    public String getKategoria() {
+        return kategoria;
+    }
+    public static int getLiczbaProduktow() {
+        return liczbaProduktow;
+    }
+
+     public boolean equals(Object obj) {
+        if (this == obj){
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+        ProduktMenu that = (ProduktMenu) obj;
+        return kod.equals(that.kod);
+    }
 }
